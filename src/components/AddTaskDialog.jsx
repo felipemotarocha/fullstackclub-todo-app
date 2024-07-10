ef54d48f6fdc918a5df5c8a9eec19e37903132c2
Felipe Rocha <felipemota.rocha@gmail.com>
2024-07-10 19:36:45 -0300

    feat: ensure inputs are clean when closing AddTaskDialog

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -1,6 +1,6 @@
 import "./AddTaskDialog.css"
 
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { createPortal } from "react-dom"
 import { CSSTransition } from "react-transition-group"
 import { v4 } from "uuid"
@@ -10,11 +10,19 @@ import Input from "./Input"
 import TimeSelect from "./TimeSelect"
 
 const AddTaskDialog = ({ isOpen, handleClose, handleSubmit }) => {
-  const [title, setTitle] = useState()
-  const [time, setTime] = useState()
-  const [description, setDescription] = useState()
+  const [title, setTitle] = useState("")
+  const [time, setTime] = useState("morning")
+  const [description, setDescription] = useState("")
 
   const nodeRef = useRef()
+
+  useEffect(() => {
+    if (!isOpen) {
+      setTitle("")
+      setTime("")
+      setDescription("")
+    }
+  }, [isOpen])
 
   const handleSaveClick = () => {
     handleSubmit({
