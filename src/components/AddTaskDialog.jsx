d5746e0caa7739dafbdae9649932aa17063e6b74
Felipe Rocha <felipemota.rocha@gmail.com>
2024-07-23 18:12:58 -0300

    chore: add prop-types to AddTaskDialog component

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -1,5 +1,6 @@
 import "./AddTaskDialog.css"
 
+import PropTypes from "prop-types"
 import { useRef, useState } from "react"
 import { createPortal } from "react-dom"
 import { CSSTransition } from "react-transition-group"
@@ -134,6 +135,12 @@ const AddTaskDialog = ({ isOpen, handleClose, handleSubmit }) => {
       </div>
     </CSSTransition>
   )
+}
+
+AddTaskDialog.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
 }
 
 export default AddTaskDialog
