212a7da98ad22e1fca0ff99b9935ac8a104df314
Felipe Rocha <felipemota.rocha@gmail.com>
2024-07-31 19:46:26 -0300

    feat: ensure task can be deleted

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import "./index.css"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { Toaster } from "sonner"
 
 import App from "./App.jsx"
 import TaskDetailsPage from "./pages/task-details.jsx"
@@ -20,6 +21,13 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Toaster
+      toastOptions={{
+        style: {
+          color: "#35383E",
+        },
+      }}
+    />
     <RouterProvider router={router} />
   </React.StrictMode>
 )
--- src/pages/task-details.jsx
+++ src/pages/task-details.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef,useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Link, useNavigate, useParams } from "react-router-dom"
 import { toast } from "sonner"
 
@@ -87,6 +87,17 @@ const TaskDetailsPage = () => {
     toast.success("Tarefa salva com sucesso!")
   }
 
+  const handleDeleteClick = async () => {
+    const response = await fetch(`http://localhost:3000/tasks/${task.id}`, {
+      method: "DELETE",
+    })
+    if (!response.ok) {
+      return toast.error("Ocorreu um erro ao deletar a tarefa.")
+    }
+    toast.success("Tarefa deletada com sucesso!")
+    navigate(-1)
+  }
+
   const titleError = errors.find((error) => error.inputName === "title")
   const timeError = errors.find((error) => error.inputName === "time")
   const descriptionError = errors.find(
@@ -121,7 +132,11 @@ const TaskDetailsPage = () => {
           </div>
 
           {/* parte da direita */}
-          <Button className="h-fit self-end" color="danger">
+          <Button
+            className="h-fit self-end"
+            color="danger"
+            onClick={handleDeleteClick}
+          >
             <TrashIcon />
             Deletar tarefa
           </Button>
